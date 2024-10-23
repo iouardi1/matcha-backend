431fd7035ad18ab940c7eb72aed0fafaf32bd457
Oussama Aissouni <oussama.aissouni@ariasc.com>
2024-10-23 17:31:25 +0100

    working on editing profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -17,7 +17,9 @@ class ProfileController {
     static async getProfileDetails(req, res) {
         const { id } = req.query
         const email = await findEmailByUserId(id)
-        const profile = await Profile.profileDetails(email)
+        const token = req.header('Authorization')?.replace('Bearer ', '')
+        const senderEmail = jwt.decode(token)
+        const profile = await Profile.profileDetails(email, senderEmail.email)
         return res.status(200).json({ data: profile, shouldRedirect: false })
     }
 
@@ -90,129 +92,129 @@ class ProfileController {
 
     static async updateProfile(req, res) {
         const token = req.header('Authorization')?.replace('Bearer ', '')
-        const { email } = jwt.decode(token)
-        const { username, firstname, lastname, gender, interests, relationship_type, interested_in_gender, bio, location, user_email, password } = req.body;
-        try {
+        const emailAddress = jwt.decode(token).email
 
-            const genderQuery = `SELECT id FROM gender g WHERE g.name = $1 LIMIT 1;`;
-            const genderResult = await db.query(genderQuery, [gender]);
-            const genderId = genderResult.rows[0]?.id;
+        const { username, firstname, lastname, gender, interests, relation_type, aboutme, email, birthday } = req.body;
+
+        console.log(gender, " | ", birthday);
+        // try {
+
+        //     const genderQuery = `SELECT id FROM gender g WHERE g.name = $1 LIMIT 1;`;
+        //     const genderResult = await db.query(genderQuery, [gender]);
+        //     const genderId = genderResult.rows[0]?.id;
             
-            if (!genderId) {
-                return res.status(400).json({ message: 'Invalid gender' });
-            }
+        //     if (!genderId) {
+        //         return res.status(400).json({ message: 'Invalid gender' });
+        //     }
             
-            let interestedInGenderId = null;
-            if (interested_in_gender) {
-                const interestedInGenderQuery = `SELECT id FROM gender WHERE name = $1 LIMIT 1`;
-                const interestedInGenderResult = await db.query(interestedInGenderQuery, [interested_in_gender]);
-                interestedInGenderId = interestedInGenderResult.rows[0]?.id;
+        //     let interestedInGenderId = null;
+        //     if (interested_in_gender) {
+        //         const interestedInGenderQuery = `SELECT id FROM gender WHERE name = $1 LIMIT 1`;
+        //         const interestedInGenderResult = await db.query(interestedInGenderQuery, [interested_in_gender]);
+        //         interestedInGenderId = interestedInGenderResult.rows[0]?.id;
                 
-                if (!interestedInGenderId) {
-                    return res.status(400).json({ message: 'Invalid interested in gender' });
-                }
-            }
+        //         if (!interestedInGenderId) {
+        //             return res.status(400).json({ message: 'Invalid interested in gender' });
+        //         }
+        //     }
             
-            // Update interested_in_gender
-            if (interestedInGenderId) {
-                const interestedInGenderQuery = `
-                UPDATE interested_in_gender
-                SET gender_id = $1
-                WHERE user_id = (SELECT id FROM users WHERE email = $2)
-                `;
-                await db.query(interestedInGenderQuery, [interestedInGenderId, email]);
-            }
+        //     if (interestedInGenderId) {
+        //         const interestedInGenderQuery = `
+        //         UPDATE interested_in_gender
+        //         SET gender_id = $1
+        //         WHERE user_id = (SELECT id FROM users WHERE email = $2)
+        //         `;
+        //         await db.query(interestedInGenderQuery, [interestedInGenderId, email]);
+        //     }
             
-            let relationshipTypeId = null;
-            if (relationship_type) {
-                const relationshipTypeQuery = `SELECT id FROM relationship_type WHERE name = $1 LIMIT 1`;
-                const relationshipTypeResult = await db.query(relationshipTypeQuery, [relationship_type]);
-                relationshipTypeId = relationshipTypeResult.rows[0]?.id;
+        //     let relationshipTypeId = null;
+        //     if (relationship_type) {
+        //         const relationshipTypeQuery = `SELECT id FROM relationship_type WHERE name = $1 LIMIT 1`;
+        //         const relationshipTypeResult = await db.query(relationshipTypeQuery, [relationship_type]);
+        //         relationshipTypeId = relationshipTypeResult.rows[0]?.id;
                 
-                if (!relationshipTypeId) {
-                    return res.status(400).json({ message: 'Invalid relationship type' });
-                }
-            }
+        //         if (!relationshipTypeId) {
+        //             return res.status(400).json({ message: 'Invalid relationship type' });
+        //         }
+        //     }
             
-            if (relationshipTypeId) {
-                const relationshipUpdateQuery = `
-                UPDATE interested_in_relation
-                SET relationship_type_id = $1
-                WHERE user_id = (SELECT id FROM users WHERE email = $2)
-                `;
-                await db.query(relationshipUpdateQuery, [relationshipTypeId, email]);
-            }
+        //     if (relationshipTypeId) {
+        //         const relationshipUpdateQuery = `
+        //         UPDATE interested_in_relation
+        //         SET relationship_type_id = $1
+        //         WHERE user_id = (SELECT id FROM users WHERE email = $2)
+        //         `;
+        //         await db.query(relationshipUpdateQuery, [relationshipTypeId, email]);
+        //     }
             
-            // Update interests
-            if (interests && interests.length > 0) {
-                // Delete current interests and insert new ones
-                const deleteInterestsQuery = `
-                DELETE FROM user_interests
-                WHERE user_id = (SELECT id FROM users WHERE email = $1)
-                `;
-                await db.query(deleteInterestsQuery, [email]);
+        //     // Update interests
+        //     if (interests && interests.length > 0) {
+        //         // Delete current interests and insert new ones
+        //         const deleteInterestsQuery = `
+        //         DELETE FROM user_interests
+        //         WHERE user_id = (SELECT id FROM users WHERE email = $1)
+        //         `;
+        //         await db.query(deleteInterestsQuery, [email]);
                 
-                const updateInterestsQuery = `
-                WITH interest_ids AS (
-                    SELECT id
-                    FROM interests
-                    WHERE name = ANY($2::text[])
-                    )
-                    INSERT INTO user_interests (user_id, interest_id)
-                    VALUES (
-                        (SELECT id FROM users WHERE email = $1),
-                        unnest(ARRAY(SELECT id FROM interest_ids))
-                        )
-                        ON CONFLICT DO NOTHING;
-                        `;
+        //         const updateInterestsQuery = `
+        //         WITH interest_ids AS (
+        //             SELECT id
+        //             FROM interests
+        //             WHERE name = ANY($2::text[])
+        //             )
+        //             INSERT INTO user_interests (user_id, interest_id)
+        //             VALUES (
+        //                 (SELECT id FROM users WHERE email = $1),
+        //                 unnest(ARRAY(SELECT id FROM interest_ids))
+        //                 )
+        //                 ON CONFLICT DO NOTHING;
+        //                 `;
                         
-                        await db.query(updateInterestsQuery, [email, interests]);
-                    }
+        //                 await db.query(updateInterestsQuery, [email, interests]);
+        //             }
                     
                     
-                    const hashedPassword = await bcrypt.hash(password, 10);
+        //             const hashedPassword = await bcrypt.hash(password, 10);
                     
                     
-                    const query = `
-                    UPDATE users
-                    SET 
-                    username = $1,
-                    firstname = $2,
-                    lastname = $3,
-                    gender_id = $4,
-                    aboutme = $5,
-                    location = $6,
-                    password = $7,
-                    email = $8
-                    WHERE email = $9
-                    `;
+        //             const query = `
+        //             UPDATE users
+        //             SET 
+        //             username = $1,
+        //             firstname = $2,
+        //             lastname = $3,
+        //             gender_id = $4,
+        //             aboutme = $5,
+        //             location = $6,
+        //             password = $7,
+        //             email = $8
+        //             WHERE email = $9
+        //             `;
                     
-                    const userUpdateValues = [username, firstname, lastname, genderId, bio, location, hashedPassword, user_email, email];
-                    const updated = await db.query(query, userUpdateValues);
+        //             const userUpdateValues = [username, firstname, lastname, genderId, aboutme, location, hashedPassword, user_email, email];
+        //             const updated = await db.query(query, userUpdateValues);
                     
-                    if (email !== user_email) {
-                        const verificationToken = uuidv4().replace(/-/g, '');
-                        const query = `
-                                UPDATE users
-                                SET 
-                                    verified_account = false,
-                                    verification_token = $1
-                                WHERE email = $2
-                            `
-                        await db.query(query, [verificationToken, user_email])
-                        await AuthController.sendVerificationEmail(user_email, verificationToken);
-                        // return res.status(200).json({ data: updated.rows[0] })
-                        return res.status(403).json({ shouldRedirect: true, redirectTo: '/auth/login' });
-                    }
+        //             if (email !== user_email) {
+        //                 const verificationToken = uuidv4().replace(/-/g, '');
+        //                 const query = `
+        //                         UPDATE users
+        //                         SET 
+        //                             verified_account = false,
+        //                             verification_token = $1
+        //                         WHERE email = $2
+        //                     `
+        //                 await db.query(query, [verificationToken, user_email])
+        //                 await AuthController.sendVerificationEmail(user_email, verificationToken);
+        //                 // return res.status(200).json({ data: updated.rows[0] })
+        //                 return res.status(403).json({ shouldRedirect: true, redirectTo: '/auth/login' });
+        //             }
                     
-                    // return res.status(200).json({ data: updated.rows[0] })
-        return res.status(200).json({ shouldRedirect: true, redirectTo: '/accueil' });
-
-
-        } catch (error){
-            console.error('Error updating profile: ', error);
-            return res.status(500).json({ message: 'Internal server error' });
-        }
+        //             // return res.status(200).json({ data: updated.rows[0] })
+        // return res.status(200).json({ shouldRedirect: true, redirectTo: '/accueil' });
+        // } catch (error){
+        //     console.error('Error updating profile: ', error);
+        //     return res.status(500).json({ message: 'Internal server error' });
+        // }
     }
 }
 
